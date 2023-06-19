7cfb41fda52255e7ecd14fc6fdc1b3e1805ce01c
amina-work <mina.works28@gmail.com>
2023-06-19 19:48:27 +0100

    stuck on the carousel for featured content

--- e-commerce/script.js
+++ e-commerce/script.js
@@ -1,3 +1,4 @@
+// ==================== HOME SLIDESHOW ====================
 const homeSection = document.querySelector('.home');
 const images = [
     './pics/homebg2.jpg',
@@ -19,3 +20,32 @@ homeSection.style.backgroundImage = `url(${images[0]})`;
 
 // Change background image every 5 seconds (5000 milliseconds)
 setInterval(changeBackgroundImage, 5000);
+
+// ==================== FEATURED CONTENT CAROUSEL ====================
+const carouselContainer = document.querySelector('.featured-content');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+let slideIndex = 0;
+const slideWidth = 100 / 4; // Assuming 4 items are displayed at a time
+
+const updateCarouselPosition = () => {
+  carouselContainer.style.transform = `translateX(-${slideIndex * slideWidth}%)`;
+};
+
+const nextSlide = () => {
+  if (slideIndex < 5) {
+    slideIndex++;
+    updateCarouselPosition();
+  }
+};
+
+const prevSlide = () => {
+  if (slideIndex > 0) {
+    slideIndex--;
+    updateCarouselPosition();
+  }
+};
+
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
