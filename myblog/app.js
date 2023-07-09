14c4d433b01893ba10dab91a21cff89951753547
amina-work <mina.works28@gmail.com>
2023-07-09 23:43:40 +0100

    prog point

--- myblog/app.js
+++ myblog/app.js
@@ -16,3 +16,22 @@ const closeNav = () => {
 }
 openNavBtn.addEventListener('click', openNav); //call the openNav function
 closeNavBtn.addEventListener('click', closeNav); //call the closeNav function
+
+
+
+
+/////////////////////////////////// Work images
+document.querySelectorAll('.works img').forEach(image =>{
+    image.onclick = () =>{
+        document.querySelector('.popup-img').style.display = 'block';
+        document.querySelector('.popup-img img').src = image.getAttribute('src');
+    }
+})
+document.querySelector('.popup-img span').onclick = () =>{
+    document.querySelector('.popup-img').style.display = 'none';
+}
+
+
+
+
+
